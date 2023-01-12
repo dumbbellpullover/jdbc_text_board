219a6259c63cf39a6ee40c1d754e2fae6b26de05
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-12 18:56:15 +0900

    JDBC 인설트 테스트

--- src/com/ukj/exam/board/test/JDBCConnectionTest.java
+++ src/com/ukj/exam/board/test/JDBCConnectionTest.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JDBCtest {
+public class JDBCConnectionTest {
   public static void main(String[] args) {
 
 
