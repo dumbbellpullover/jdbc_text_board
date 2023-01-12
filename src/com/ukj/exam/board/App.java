31e6338da8ab0b00696aa10259f9542a57bf30b8
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-12 19:12:54 +0900

    게시물 작성시 db에 저장

--- src/com/ukj/exam/board/App.java
+++ src/com/ukj/exam/board/App.java
@@ -1,5 +1,9 @@
 package com.ukj.exam.board;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -30,11 +34,61 @@ public class App {
 
       } else if (cmd.equals("add")) {
         System.out.println("\n== 게시물 생성 ==");
-        int id = ++articleLastId;
         System.out.printf("제목: ");
         String title = sc.nextLine();
         System.out.printf("내용: ");
         String body = sc.nextLine();
+        int id = ++articleLastId;
+
+        Connection conn = null;
+        PreparedStatement pstat = null;
+
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "guest", "bemyguest");
+
+          // 쿼리 작성 부분
+
+          String query = "insert into article (regDate, updateDate, title, body) \n" +
+              "values (now(), now(), \"" + title + "\", \"" + body + "\");";
+
+          pstat = conn.prepareStatement(query);
+          int affectedRows = pstat.executeUpdate();
+
+          System.out.println("affectedRows: " + affectedRows);
+
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+
+        } catch (SQLException e) {
+          System.out.println("에러: " + e);
+
+
+        } finally { // Connection, PreparedStatement 객체는 garbage collector가 수거하지 않아, 직접 메모리해제를 해줘야 한다.
+          try {
+            if (conn != null && !conn.isClosed()) {
+              conn.close();
+
+            }
+
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+
+          try {
+            if (pstat != null && !pstat.isClosed()) {
+              pstat.close();
+
+            }
+
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+
+        }
 
         Article article = new Article(id, title, body);
         articles.add(article);
@@ -42,7 +96,7 @@ public class App {
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물을 생성하였습니다.\n", article.id);
 
-      } else if (cmd.equals("system exit")) {
+      } else if (cmd.equals("exit")) {
         System.out.println("시스템 종료");
         break;
 
